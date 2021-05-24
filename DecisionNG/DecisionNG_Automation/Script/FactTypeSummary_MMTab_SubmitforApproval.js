f8d8ed9a91a53ae0296428bf7f6b12fee1ef43bf
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-05-24 18:19:13 +0530

    Test

--- DecisionNG/DecisionNG_Automation/Script/FactTypeSummary_MMTab_SubmitforApproval.js
+++ DecisionNG/DecisionNG_Automation/Script/FactTypeSummary_MMTab_SubmitforApproval.js
@@ -3,6 +3,6 @@
 //  Aliases.browser.pageSapiensDecision2.panel19.Click();
 //  Aliases.browser.pageSapiensDecision2.panel20.Click();
 //  Aliases.browser.pageSapiensDecision2.panel15.Click();
- Aliases.browser.pageSapiensDecision2.textnode14.linkSubmitForApproval.textnodeSubmitForApproval.Click();
+    Aliases.browser.pageSapiensDecision2.textnode14.linkSubmitForApproval.textnodeSubmitForApproval.Click();
  
 }
