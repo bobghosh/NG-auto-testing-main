fcf41f6822600fe88d5f90927776efa9e3fe5a8b
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-06-03 14:57:36 +0530

    ModelMapping_1st Commit

--- DecisionNG/DecisionNG_Automation/Script/FactTypeSummary_MMTab_SubmitforApproval.js
+++
@@ -1,8 +0,0 @@
-﻿function FactTypeSummary_MMTab_SubmitforApproval()
-{
-//  Aliases.browser.pageSapiensDecision2.panel19.Click();
-//  Aliases.browser.pageSapiensDecision2.panel20.Click();
-//  Aliases.browser.pageSapiensDecision2.panel15.Click();
-    Aliases.browser.pageSapiensDecision2.textnode14.linkSubmitForApproval.textnodeSubmitForApproval.Click();
- 
-}
