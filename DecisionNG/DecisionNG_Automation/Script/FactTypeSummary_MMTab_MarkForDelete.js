1c125b8e436fcfc8d1a65f20ab798e82e63396b0
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-06-09 12:14:12 +0530

    Model Mapping Code

--- DecisionNG/DecisionNG_Automation/Script/FactTypeSummary_MMTab_MarkForDelete.js
+++
@@ -1,6 +0,0 @@
-﻿function FactTypeSummary_MMTab_MarkForDelete()
-{
-  Aliases.browser.pageSapiensDecision2.FindElement("//*[contains(@style,'visibility: visible; display: block')]//*[text()='Mark for Delete']").Click();
-//  aqObject.CheckProperty(Aliases.browser.pageSapiensDecision2.textnode15, "Exists", cmpEqual, true);
-//  aqObject.CheckProperty(Aliases.browser.pageSapiensDecision2.textnode15, "className", cmpEqual, "icon-bin spec-trash-icon ng-star-inserted");
-}
