126f56d11395219b3e335e2ef31f5c082245ccd7
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-08-19 15:23:29 +0530

    Merging_Validation Button

--- DecisionNG/DecisionNG_Automation/Script/Unit2.js
+++
@@ -1,6 +0,0 @@
-﻿function test()
-{
-  let page= Aliases.browser.pageSapiensDecision2
-  let child = page.FindElement("//*[@class='valid-value-combobox-spec']//div/div/div").ChildCount
-  Log.Message(child)
-}
