53ce6a0013eea27f709b6049843219c40c516b47
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-09-30 17:50:03 +0530

    Permissioncheck commit

--- DecisionNG/DecisionNG_Automation/Script/Permission_Matrix_Set_Permission_PermissionGroups.js
+++ DecisionNG/DecisionNG_Automation/Script/Permission_Matrix_Set_Permission_PermissionGroups.js
@@ -1,22 +1,31 @@
-﻿ 
- let super_array =[];
- let app_array =[];
+﻿let super_array =[];
  let row_array=[];
+
 function SelectingItemGetData ()
 {
+   
   let columnCount=Project.Variables.Permission_Matrix.ColumnCount;
   Log.Message(columnCount)
-  let app_subarray=[];
+  
   let row_subarray=[];
   var ItemCount;
   var flag = 0;
   hasNext = true;
   
   do
-  {    
-    
-    Aliases.browser.pageSapiensDecision.FindElement(".ui-dropdown-trigger").Click();   
-    Aliases.browser.pageSapiensDecision.FindElement("//li[contains(., '50')]").Click();
+  { 
+    var Paginator = Aliases.browser.pageSapiensDecision.FindElements("p-dropdown");
+    //Checking for Pagination
+    if(Paginator>0)
+    {
+      Aliases.browser.pageSapiensDecision.FindElement(".ui-dropdown-trigger").Click();
+      Aliases.browser.pageSapiensDecision.FindElement("//li[contains(., '100')]").Click();
+    }
+    else
+    {
+      Log.Message("Pagination is not present")
+    }
+
     TotalRows = Aliases.browser.pageSapiensDecision.FindElements("//tbody/tr");
     
     //Iterate through all the rows and finding the desired Task
@@ -32,7 +41,6 @@ function SelectingItemGetData ()
     row_array.push(row_subarray);
     var rowarray= new Array();
     rowarray=row_array.toString().split(",");  
-    //Log.Message("This is rowarray"+row_array)
     
      for(let c=1;c<columnCount;c++)
     {
@@ -47,12 +55,10 @@ function SelectingItemGetData ()
     //Iterate through all the rows and finding the desired Task
     for(var j = 1; j <= ItemCount.length ; j++)
     {
-      
+      let app_subarray=[];
+      let app_array =[];
       var HighlightedItemName = Aliases.browser.pageSapiensDecision.FindElement("//tbody/tr["+j+"]/td[1]//a");
-      //var HighlightedCommunityName = Aliases.browser.pageSapiensDecision.FindElement("//tbody/tr["+ j + "]/td[2]");
-    
-      //If the Item Name matches 
-      //Log.Message(cname)
+      
       if(cname == HighlightedItemName.textContent)
       {     Log.Message(HighlightedItemName.textContent)
             Log.Message(cname)
@@ -61,20 +67,18 @@ function SelectingItemGetData ()
             for(var k=1;k <=permissions.length; k++)
             { 
               let text=Aliases.browser.pageSapiensDecision.FindElement("//tbody//tr["+j+"]//td[2]//div//div//div["+k+"]").textContent
-              var txtval=text.toString().replace(/_/g,"").trim();     
+              var txtval=text.toString().trim();     
               app_subarray.push(txtval)
               flag =1;
 //  
             }
            
              app_array.push(app_subarray);
-             let sub_array = [];
-    var newarr= new Array();
-    newarr=app_array.toString().split(",");  
-    //Log.Message(newarr)
-      
-     
    
+    var newarr= new Array();
+    newarr=app_array.toString().split(","); 
+      
+   let pervalarray=[];
     Project.Variables.Permission_Matrix.Reset();
     for(; ! Project.Variables.Permission_Matrix.IsEOF();)
     {
@@ -82,11 +86,11 @@ function SelectingItemGetData ()
       //Project.Variables.Permission_Matrix.ColumnName()
       if(Project.Variables.Permission_Matrix.Value(cname) =="ü")
       {
-        sub_array.push(Project.Variables.Permission_Matrix.Value("Permissions"));
-        //Log.Message(Project.Variables.Permission_Matrix.Value("Permissions"));
         let PermissionValue=Project.Variables.Permission_Matrix.Value("Permissions");
-        let perval=PermissionValue.toString().replace(/_/g,"").trim();
-        //Log.Message("after replace"+perval)
+        let perval=PermissionValue.toString().trim();
+        pervalarray.push(perval);
+        var excel_arr=new Array();
+        excel_arr=pervalarray.toString().split(","); 
         
         if (!newarr.includes(perval))
         {
@@ -109,11 +113,29 @@ function SelectingItemGetData ()
       
        Project.Variables.Permission_Matrix.Next();
     }
+    let dif1 = newarr.diff(excel_arr);  
+    let differ_arr=new Array();
+    differ_arr=dif1.toString().split(",");
+    
+    Log.Message(excel_arr.length)
+    Log.Message(newarr.length)    
+    
+    if(newarr.length>excel_arr.length)
+    {
+     HighlightedItemName.click();
+     for(m=0; m<differ_arr.length; m++)
+     {
+       Log.Message(differ_arr[m]) 
+       Aliases.browser.pageSapiensDecision.FindElement("//ul//li[@aria-label='"+differ_arr[m]+"']//*[@class='ui-chkbox ui-widget ng-star-inserted']").click(); 
+     }
+     Aliases.browser.pageSapiensDecision.form.buttonOk.Click();            
+    }
     
     }
 
     }
     }
+    
     else
     {
       
@@ -144,6 +166,7 @@ function SelectingItemGetData ()
       
 
     }
+    
     }
     }
      if(flag == 1)
@@ -166,5 +189,10 @@ function SelectingItemGetData ()
 
         
     }while (hasNext==true)
+    
 
 }
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
