37635d74433e43a431c8785fae82893140910924
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-11-10 13:32:04 +0530

    Additional Info TestCase_nav_Repo

--- DecisionNG/DecisionNG_Automation/Script/DRF_DBLClick_Open.js
+++ DecisionNG/DecisionNG_Automation/Script/DRF_DBLClick_Open.js
@@ -3,7 +3,7 @@
   
   OCR.Recognize(Aliases.browser.pageSapiensDecision2.canvas).BlockByText(DRFName).ClickNextTo(toBottom, 20)
   let x= Sys.Desktop.MouseX
-  let y = Sys.Desktop.MouseY-50;
+  let y = Sys.Desktop.MouseY-100;
   Log.Message(x)
   Log.Message(y)
   Aliases.browser.pageSapiensDecision2.DblClick(x,y);
