1039e83e62b67cfba5e560471f33d4f21c1aaefc
Sumant.Pattanshetti <sumant.pattanshetti@sapiens.com>
2021-12-28 14:55:08 +0530

    Close Browser Added

--- DecisionNG/DecisionNG_Automation/Script/CloseBrowsers.js
+++ DecisionNG/DecisionNG_Automation/Script/CloseBrowsers.js
@@ -1,7 +1,26 @@
-﻿function Close()
+﻿var Picture_To_Log = require("Picture_To_Log");
+function Close()
 {
 
-  while (Sys.WaitBrowser().Exists)
-    Sys.WaitBrowser().Close();
+//  while (Sys.WaitBrowser().Exists)
+//    Sys.WaitBrowser().Close();
+//    if(Sys.Browser("Chrome"))
+//    {
+//      Sys.Browser("Chrome").Close();
+//      Log.Message("Chrome Exists")
+//    }
+
+ var PropArray, ValuesArray;
+PropNames = new Array("processname", "index");
+
+  PropValues = new Array("chrome", 1);
+
+
+
+  var page = Sys.FindChild(PropNames, PropValues, 1);
+
+  if(page.Exists)  
+  page.Close();
+  Picture_To_Log.PictureToLog();
 
 }
